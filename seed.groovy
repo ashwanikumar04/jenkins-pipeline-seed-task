5c5736b9fa3b1addc0b5aba1a140159d94f00eb0
Ashwani Kumar <ashwanikumar04@gmail.com>
2018-11-05 23:21:09 +0530

    Build scripts

--- seed.groovy
+++ seed.groovy
@@ -12,25 +12,3 @@ stages {
         }
     }
 }
-
-pipeline {
-    agent { label 'linux' }
-
-    parameters {
-        booleanParam(name: "GO?", defaultValue: true,
-            description: "Dummy parameter to prevent running the job just by one click only.")
-    }
-
-    stages {
-        // NOTE for all 'jobDsl' call:
-        //   We MUST combine all scripts in one call in order for the "removedJobAction: 'DISABLE'"
-        //   parameter; otherwise latter running dsl calls will disable the jobs created by earlier acctions.
-        stage('Create All Jobs') {
-            steps {
-                jobDsl sandbox: true, removedJobAction: 'DISABLE', targets: """
-seed.dsl
-"""
-            }
-        }
-    }
-}
